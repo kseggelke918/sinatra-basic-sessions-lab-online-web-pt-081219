1aa390f058e14b53e00819dc5cb27bed16bd1a4a
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-18 21:29:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+require 'securerandom'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions 
+    set :session_secret, "secret_code"
+  end 
+  
+  get '/' do 
+    erb :index 
+  end 
+  
+  post '/checkout' do 
+    @item = params[:item]
+    @session = session 
+    erb :checkout 
+  end 
+  
 end
